f2e8fa4f6ee627756c35ba7773775674a2264808
LTam4s <114070808+LTam4s@users.noreply.github.com>
2023-01-16 14:19:19 +0100

    Add project files.

---
+++ ParancsgombHatterenekMegvaltoztatasa/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ParancsgombHatterenekMegvaltoztatasa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_gomb = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel_gomb
+            // 
+            this.panel_gomb.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel_gomb.Location = new System.Drawing.Point(141, 0);
+            this.panel_gomb.Name = "panel_gomb";
+            this.panel_gomb.Size = new System.Drawing.Size(659, 450);
+            this.panel_gomb.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ParancsgombHatterenekMegvaltoztatasa.Properties.Resources.maxresdefault;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel_gomb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_gomb;
+    }
+}
+
---
+++ ParancsgombHatterenekMegvaltoztatasa/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParancsgombHatterenekMegvaltoztatasa
+{
+    public partial class Form1 : Form
+    {
+        int sorokSzama = 3;
+        int oszlopokSzama = 6;
+        int pictureBoxSize = 60;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < sorokSzama; i++)
+            {
+                for (int j = 0; j < oszlopokSzama; j++)
+                {
+                    PictureBox pb = new PictureBox();
+                    pb.SetBounds(j * pictureBoxSize, i * pictureBoxSize, pictureBoxSize, pictureBoxSize);
+                    pb.Text = (i * oszlopokSzama + j).ToString();
+                    pb.BackColor = Color.LightGray;
+                    pb.Click += (o, ev) =>
+                    {
+                        PictureBox pic = (PictureBox)o;
+                        pic.BackgroundImage = Properties.Resources._3b921c51dc99d9fb2be192af3ec14f72;
+                        pic.BackgroundImageLayout = ImageLayout.Zoom;
+                    };
+                    panel_gomb.Controls.Add(pb);
+                }
+            }
+        }
+    }
+}
